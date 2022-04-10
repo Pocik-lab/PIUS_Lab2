de25ca6d38f22a279810e516ed085ec610da5fa3
Pocik-lab <tegin2011@gmail.com>
2022-04-10 18:22:57 +0300

    task 3.2 in progress

--- u8191560-lab2/app/Http/Controllers/BuyerController.php
+++ u8191560-lab2/app/Http/Controllers/BuyerController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Buyer;
 
 class BuyerController extends Controller
 {
-    //
+    public function index()
+    {
+        $customers = Buyer::all();
+        return view('customers', compact('customers'));
+    }
 }
